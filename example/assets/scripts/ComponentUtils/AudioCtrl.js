d8f58364db8d0148226eadb7b10ba2292582f826
nusashi <nusashi@gmail.com>
2020-01-29 00:40:43 +0900

    no message

--- example/assets/scripts/ComponentUtils/AudioCtrl.js
+++ example/assets/scripts/ComponentUtils/AudioCtrl.js
@@ -1,54 +1,56 @@
+'use strict';
+
 cc.Class({
-    extends: cc.Component,
+	extends: cc.Component,
 
-    properties: {
-        timeLabel: {
-            default: null,
-            type: cc.Label
-        },
-        _audioTask: null,
-        _audioID: null,
-    },
+	properties: {
+		timeLabel: {
+			default: null,
+			type: cc.Label
+		},
+		_audioTask: null,
+		_audioID: null
+	},
 
-    setAudioTask (audio) {
-        this._audioTask = audio;
-    },
+	setAudioTask: function(audio) {
+		this._audioTask = audio;
+	},
 
-    playAudio () {
-        // return current audio object
-        this._audioID = cc.audioEngine.play(this._audioTask, false);
-    },
+	playAudio: function() {
+		// return current audio object
+		this._audioID = cc.audioEngine.play(this._audioTask, false);
+	},
 
-    stopAudio () {
-        cc.audioEngine.stop(this._audioID);
-    },
+	stopAudio: function() {
+		cc.audioEngine.stop(this._audioID);
+	},
 
-    pauseAudio () {
-        cc.audioEngine.pause(this._audioID);
-    },
+	pauseAudio: function() {
+		cc.audioEngine.pause(this._audioID);
+	},
 
-    resumeAudio () {
-        cc.audioEngine.resume(this._audioID);
-    },
+	resumeAudio: function() {
+		cc.audioEngine.resume(this._audioID);
+	},
 
-    stopAllAudio () {
-        cc.audioEngine.stopAll();
-    },
+	stopAllAudio: function() {
+		cc.audioEngine.stopAll();
+	},
 
-    pauseAllAudio () {
-        cc.audioEngine.pauseAll();
-    },
+	pauseAllAudio: function() {
+		cc.audioEngine.pauseAll();
+	},
 
-    resumeAllAudio () {
-        cc.audioEngine.resumeAll();
-    },
+	resumeAllAudio: function() {
+		cc.audioEngine.resumeAll();
+	},
 
-    currentTime () {
-        if (!this._audioTask || this._audioID === null) {
-            return;
-        }
-        let currentTime = cc.audioEngine.getCurrentTime(this._audioID).toFixed(1);
-        let durationTime = cc.audioEngine.getDuration(this._audioID).toFixed(1);
-        this.timeLabel.string = `${currentTime} s / ${durationTime} s`;
-    },
+	currentTime: function() {
+		if (!this._audioTask || this._audioID === null) {
+			return;
+		}
+		var currentTime = cc.audioEngine.getCurrentTime(this._audioID).toFixed(1);
+		var durationTime = cc.audioEngine.getDuration(this._audioID).toFixed(1);
+		this.timeLabel.string = '${currentTime} s / ${durationTime} s';
+	}
 });
